5e6578d23a2cd9d75342243015e756b464eac012
tomKPZ <tomKPZ@gmail.com>
2022-03-13 14:31:11 -0700

    reply

--- pykak.py
+++ pykak.py
@@ -10,6 +10,9 @@ def main():
     parser.add_argument('kak2py', type=str)
     parser.add_argument('py2kak', type=str)
     args = parser.parse_args()
+
+    def reply(response):
+        open(args.py2kak, 'w').write(response)
     while True:
         request = textwrap.dedent(open(args.kak2py).read())
         exec(request)
