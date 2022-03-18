555c1f10cf7d8635979cedfddc76776bf107d08a
tomKPZ <tomKPZ@gmail.com>
2022-03-18 08:06:15 -0700

    Revert threaded code
    It turns out the threaded code is actually slower than just syncing.
    
    Revert "add todo"
    
    This reverts commit 06a1330c998ac653a008185ef776c92ee99ef4a2.
    
    Revert "fix potential deadlock"
    
    This reverts commit bb3456869b713c1f3dd1319a9e1fa891648afc9a.
    
    Revert "extract"
    
    This reverts commit f2bc01bdaacee7e5aebdd78b04de19f2d8841738.
    
    Revert "better error messages"
    
    This reverts commit ae6cea7a03545b34eecb04b9b4f97adf3ad3878b.
    
    Revert "fix read_queue task_done"
    
    This reverts commit 6ff3bde38532868706b762ee3cd3e50bf7b38f9e.
    
    Revert "drain unread data"
    
    This reverts commit 6751b3a266e4a5be3009151ff93e905d7900055c.
    
    Revert "sync"
    
    This reverts commit 5155cd6bef5f207cf19ced69431fad589fc76712.
    
    Revert "reorder"
    
    This reverts commit 10c1f29b8a8a444329f7d2b0e2e127a7aaa6018c.
    
    Revert "add read queue"
    
    This reverts commit cbeb4719b1fa95866e8baa114eafbffc198b9b6e.
    
    Revert "remove some write queue code"
    
    This reverts commit 492ec0ad1df9701a7c4f83a416dfdadbe9700ba7.
    
    Revert "fix a deadlock"
    
    This reverts commit 4786620240a4dbfd7d1eb48250d2d7531793dabd.

--- pykak.py
+++ pykak.py
@@ -2,85 +2,12 @@
 
 import argparse
 import itertools
-import queue
 import textwrap
-import threading
 import traceback
 
 
 class KakException(Exception):
     pass
-
-
-class DoneToken:
-    pass
-
-
-def _drain_read_queue():
-    assert _io_lock.locked()
-    data = []
-    try:
-        while True:
-            data.append(_read_queue.get_nowait())
-    except queue.Empty:
-        pass
-    return data
-
-
-def _echo_error(message):
-    message = message.replace('"', '""')
-    return ('echo -markup "{Error}{\\}pykak error: '
-            'see *debug* buffer"; ' +
-            ('echo -debug "pykak error: %s"' % message))
-
-
-def _format_write_data(data):
-    if type(data) == DoneToken:
-        data = ''
-        unhandled = _drain_read_queue()
-        if unhandled:
-            message = 'unhandled messages: '
-            message += '\n'.join(unhandled)
-            data = _echo_error(message)
-            data += '; '
-        data += 'alias global pk_done nop'
-    return data
-
-
-def _write_inf():
-    for fname in itertools.cycle([_args.py2kaka, _args.py2kakb]):
-        data = _write_queue.get()
-        with open(fname, 'w') as f:
-            with _io_lock:
-                f.write(_format_write_data(data))
-                _write_queue.task_done()
-
-
-def _read_inf():
-    for fname in itertools.cycle([_args.kak2pya, _args.kak2pyb]):
-        with open(fname, 'r') as f:
-            # TODO: There's a race condition between the open above and
-            # obtaining the lock below.
-            with _io_lock:
-                _read_queue.put(f.read())
-
-
-def _read():
-    data = _read_queue.get()
-    if data.startswith(_ERROR_UUID):
-        raise KakException(data.removeprefix(_ERROR_UUID))
-    return data
-
-
-def _write(response):
-    _write_queue.put(response)
-
-
-def _getter(prefix):
-    def getter_impl(name):
-        _write('pk_write "%%%s{%s}"' % (prefix, name))
-        return _read()
-    return getter_impl
 
 
 _ERROR_UUID = 'f74c66de-3e90-4ee5-ae33-bf7e8e358cb0'
@@ -92,30 +19,42 @@ _parser.add_argument('py2kaka', type=str)
 _parser.add_argument('py2kakb', type=str)
 _args = _parser.parse_args()
 
-_write_queue = queue.Queue()
-_read_queue = queue.Queue()
+_kak2py = itertools.cycle([_args.kak2pya, _args.kak2pyb])
+_py2kak = itertools.cycle([_args.py2kaka, _args.py2kakb])
 
-_io_lock = threading.Lock()
 
-_write_thread = threading.Thread(target=_write_inf)
-_write_thread.start()
-_read_thread = threading.Thread(target=_read_inf)
-_read_thread.start()
+def _write(response):
+    with open(next(_py2kak), 'w') as f:
+        f.write(response)
+
+
+def _read():
+    with open(next(_kak2py), 'r') as f:
+        data = f.read()
+    if data.startswith(_ERROR_UUID):
+        raise KakException(data.removeprefix(_ERROR_UUID))
+    return data
+
+
+def _getter(prefix):
+    def getter_impl(name):
+        _write('pk_write "%%%s{%s}"' % (prefix, name))
+        return _read()
+    return getter_impl
+
 
 arg = _getter('arg')
 opt = _getter('opt')
 reg = _getter('reg')
 val = _getter('val')
 
+
 while True:
     try:
-        try:
-            data = _read()
-        except:
-            assert False
-        exec(textwrap.dedent(data))
+        exec(textwrap.dedent(_read()))
     except:
         exc = traceback.format_exc().replace('"', '""')
-        _write(_echo_error(exc))
-    _write(DoneToken())
-    _write_queue.join()
+        _write('echo -markup "{Error}{\\}pykak error: '
+               'see *debug* buffer"')
+        _write('echo -debug "pykak error: %s"' % exc)
+    _write('alias global pk_done nop')
